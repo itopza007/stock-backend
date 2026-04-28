2266dbd1bbbbe57b0bd69ad08dae00f85cd7e3b2
itopza007 <141712159+itopza007@users.noreply.github.com>
2026-04-28 17:24:02 +0700

    update9

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ cloudinary.config({
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 /* ════════════════════════════════════════
    Multer
